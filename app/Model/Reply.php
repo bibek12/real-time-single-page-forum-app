5247c2df609eb51d2bb748b7ead745e2788d1dc6
bibek12 <sbibek100@gmail.com>
2019-03-01 18:53:44 +0545

    Relationship between user question and reply added

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,24 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Question;
+use App\Model\Like;
 
 class Reply extends Model
 {
     //
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
+
 }
